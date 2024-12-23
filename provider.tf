1d589c46bb1a80306f799f8e8e527ab2898822d4
Muhammad Saqib <muhammad.saqib@eurustechnologies.com>
2024-12-23 16:50:31 +0500

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -7,6 +7,33 @@ terraform {
   }
 }
 
+variable "subscription_id" {
+  description = "Azure Subscription ID"
+  type        = string
+}
+
+variable "client_id" {
+  description = "Azure Client ID (Application ID)"
+  type        = string
+}
+
+variable "client_secret" {
+  description = "Azure Client Secret (Application Secret)"
+  type        = string
+  sensitive   = true
+}
+
+variable "tenant_id" {
+  description = "Azure Tenant ID"
+  type        = string
+}
+
+
 provider "azurerm" {
   features {}
+
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
 }
