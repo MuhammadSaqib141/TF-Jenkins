db6d04c9d21793bdd81bedb77865a04149454447
Muhammad Saqib <muhammad.saqib@eurustechnologies.com>
2024-12-23 16:59:06 +0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -48,6 +48,8 @@ resource "azurerm_linux_virtual_machine" "example" {
   resource_group_name = azurerm_resource_group.example.name
   location            = azurerm_resource_group.example.location
   size                = "Standard_F2"
+  disable_password_authentication = false
+
   admin_username      = "adminuser"
   admin_password      = var.admin_password != "" ? var.admin_password : random_password.example.result
   network_interface_ids = [
